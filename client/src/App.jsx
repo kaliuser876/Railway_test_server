e946999ef2252ac253b3c85c33f569c2bfbc5f14
kaliuser876 <jonrrich112@gmail.com>
2025-10-27 03:42:49 -0500

    updating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import axios from "axios"
+
 
 function App() {
   const [count, setCount] = useState(0)
