a23ac73e4d9f286e16fcb1b3aaa3ad9213254468
kaliuser876 <jonrrich112@gmail.com>
2025-10-27 02:57:59 -0500

    The beginings of a full-stack project

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require("express")
-const app = express
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+    orgin: "http://localhost:5173",
+};
+app.use(cors(corsOptions));
 
-app.length("/api", (req,res) => {
-    res.json({fruites: ["apple", "orange", "bannana"]});
+
+app.get("/api", (req,res) => {
+    res.json({fruits: ["apple", "strawberry", "banana"]});
 });
 
 app.listen(8080, () =>{
     console.log("Server started on port 8080");
-})
+});
