2887a06b9bba2b4491e0631cf23f963f9cee7653
kaliuser876 <jonrrich112@gmail.com>
2025-10-31 00:51:51 -0500

    updated

--- client/front-end/src/App.jsx
+++ client/front-end/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -25,7 +24,6 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
